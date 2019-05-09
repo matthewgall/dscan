3a4818e8472b40aaa7cbfa91180a8d5f6009cd1b
Matthew Gall <git@matthewgall.com>
2019-05-09 20:33:46 +0100

    RELEASE 0.0.2
    Removed un-needed logging functionality, and added file ops

--- dscan/dscan.py
+++ dscan/dscan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys, argparse, logging
+import os, sys, argparse, json
 import requests
 
 def single(domain, rrtype):
@@ -37,12 +37,6 @@ def main():
 	parser.add_argument("--verbose", "-v", help="increase output verbosity", action="store_true")
 	args = parser.parse_args()
 
-	if args.verbose:
-		logging.basicConfig(level=logging.DEBUG)
-	else:
-		logging.basicConfig(level=logging.INFO)
-	log = logging.getLogger(__name__)
-
 	if args.single:
 		print("\n".join(single(args.domain, args.rrtype)))
 	
@@ -52,7 +46,15 @@ def main():
 		with open(args.input, 'r') as f:
 			for d in f.readlines():
 				domains.append(d.strip())
+			f.close()
+
+		output = {}
+		for d in domains:
+			output[d] = single(d, args.rrtype)
 		
+		with open(args.output, 'w') as f:
+			f.write(json.dumps(output))
+			f.close()
 		
 if __name__ == '__main__':
 	main()
