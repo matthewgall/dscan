d100b0c7d7ed6264fdd074ea5dfa03d61d97f295
Matthew Gall <git@matthewgall.com>
2019-05-11 15:30:03 +0100

    RELEASE 0.0.6
    Improved error handling in exceptions and some changes to add more providers

--- dscan/dscan.py
+++ dscan/dscan.py
@@ -12,24 +12,31 @@ class dscan:
 		if not self.provider in ["cloudflare", "google"]:
 			self.provider = "cloudflare"
 		
-		if self.provider in ['cloudflare']:
-			self.base = "https://cloudflare-dns.com/dns-query"
-		if self.provider in ['google']:
-			self.base = "https://dns.google.com/resolve"
+		providers = {
+			'cloudflare': "https://cloudflare-dns.com/dns-query",
+			'google': 'https://dns.google.com/resolve'
+		}
+		self.base = providers[self.provider]
 
 	def single(self, domain, rrtype='AAAA'):
 		if not rrtype.upper() in ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'TXT']:
 			return "You provided an invalid rrtype: {}".format(rrtype)
 
-		data = requests.get(
-			"{}?name={}&type={}".format(self.base, domain, rrtype),
-			headers=self.headers
-		)
-		if data.status_code == 200:
-			data = data.json()
-			if data['Status'] == 0:
-				results = []
-				for answer in data['Answer']:
-					results.append(answer['data'])
-				return results
-		return None
+		try:
+			data = requests.get(
+				"{}?name={}&type={}".format(self.base, domain, rrtype),
+				headers=self.headers
+			)
+			if data.status_code == 200:
+				data = data.json()
+				if data['Status'] == 0:
+					results = []
+					try:
+						for answer in data['Answer']:
+							results.append(answer['data'])
+						return results
+					except:
+						return []
+			return []
+		except:
+			return []
